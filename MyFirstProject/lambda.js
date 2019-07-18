e50be4b7afe0ea6444545342bb2ac07dccff7c28
Kumudikatest <kumudikarupasinghe+adrt@gmail.com>
2019-07-18 12:01:39 +0530

    test

--- MyFirstProject/lambda.js
+++ MyFirstProject/lambda.js
@@ -1,5 +1,4 @@
-exports.handler = function (event, context, callback) {
-
-
-    callback(null, { "message": "Successfully executed" });
+exports.handler = function(event, context, callback) {
+    
+    callback(null, {"message": "Successfully executed"});
 }
