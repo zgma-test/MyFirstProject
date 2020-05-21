ae44f2be9db898be1422c0f5b14c487a34c5279e
Kumudikatest <kumudikarupasinghe+adrt@gmail.com>
2020-05-21 16:39:26 +0530

    smoke

--- MyFirstProject.js
+++ MyFirstProject.js
@@ -1,15 +1,5 @@
-let AWS = require('aws-sdk');
-const ddb = new AWS.DynamoDB.DocumentClient();
-
 exports.handler = async (event) => {
-    try {
-        let data = await ddb.query({
-            TableName: "jnk"
-        }).promise();
-
-    } catch (err) {
-        // error handling goes here
-    };
+    debugger;
 
     return { "message": "Successfully executed" };
 };
