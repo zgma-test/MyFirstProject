2478edbc73d87b72c326ae9d4ba9cd2bc7b1a954
Kumudikatest <kumudikarupasinghe+adrt@gmail.com>
2020-10-06 17:07:28 +0530

    tes

--- k.js
+++ k.js
@@ -1,4 +1,4 @@
 exports.handler = async (event) => {
-    console.log("Test")
+    
     return {"message": "Successfully executed"};
 };
---
+++ sub/dew.js
@@ -0,0 +1,5 @@
+    exports.handler = async (event) => {
+        console.log("Test1");
+        console.log("Test2");
+        return {"message": "Successfully executed"};
+};
